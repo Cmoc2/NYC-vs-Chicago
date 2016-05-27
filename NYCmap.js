7efe5553bb2d62f0bbcb734d0752a8faba0f9e24
Ismael Lopez <ilope10@ucsc.edu>
2016-05-26 17:34:54 -0700

    Add files via upload

--- NYCmap.js
+++ NYCmap.js
@@ -2,6 +2,77 @@
 var width = 600,
     height = 600;//800x600
 
+
+//an SVG for New York
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+d3.json("NYData.json", function(error, json) {
+    if (error) return console.error(error);
+    console.log(json.objects.features.geometries);
+    
+    var features = topojson.feature(json,json.objects.features);
+    var projection = d3.geo.mercator()
+  					.center([-73.94, 40.70])
+  					.scale(54000)
+  					.translate([(width) / 2, (height)/2]);
+    
+    var path = d3.geo.path().projection(projection);
+    svg.append("path")
+        .datum(features)
+        .attr("d", path);
+    
+    //space for tooltip
+    var tooltip = d3.select("body")
+	.append("div")
+    .attr("class","tooltip")
+	.style("position", "absolute")
+	.style("z-index", "10")
+    .style("opacity",0)
+	.style("display", "none");
+    
+    //color the Areas
+    svg.selectAll(".features")
+    .data(topojson.feature(json, json.objects.features).features)
+  .enter().append("path")
+    .attr("class", "NYfeatures")
+    .attr("d", path)
+    .on("mouseover", function(d){
+        tooltip.transition()
+        .duration(200)
+        .style("opacity", .9);
+        //change the details inside the tooltip
+        //if showing general information:
+        if(!DetailedTooltip) {
+            tooltip.html("Borough: "+d.properties.boro_name+"<br/>"+ "Community District: "+ d.id+"<br/>"
+                    +"Population: "+d.properties.population+"<br/>"
+                    +"Life Expectancy: "+d.properties.lifeExpectancy+"<br/>"
+                    +"Income per Capita: "+d.properties.income+"<br/>"
+                    +"Crime: "+d.properties.crimePerK);
+        } else //else show details on topic.
+            tooltip.html("Detailed Info");
+        return tooltip.style("display","inline");
+    })
+    .on("click", function(d){
+        DetailedTooltip=!DetailedTooltip; //toggle.
+        if(!DetailedTooltip) {
+            tooltip.html("Borough: "+d.properties.boro_name+"<br/>"+ "Community District: "+ d.id+"<br/>"
+                    +"Population: "+d.properties.population);
+        } else tooltip.html("Detailed Info");
+    })
+    .on("mousemove", function(d){
+        return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");
+    })
+    .on("mouseout", function(d){
+        tooltip.transition()
+        .duration(500)
+        .style("opacity", 0);
+       //return tooltip.style("visibility","hidden");
+    });
+});
+
+
 //SVG for Chicago
 var svg2= d3.select("body").append("svg")
     .attr("width", width)
@@ -83,75 +154,9 @@ d3.json("ChicagoData.json", function(error, json) {
     return [d3.min(data, function(i){return i.properties[toGet];}),d3.max(data, function(i){return i.properties[toGet];})];
 }
 });
-//an SVG for New York
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
 
-d3.json("NYData.json", function(error, json) {
-    if (error) return console.error(error);
-    console.log(json.objects.features.geometries);
-    
-    var features = topojson.feature(json,json.objects.features);
-    var projection = d3.geo.mercator()
-  					.center([-73.94, 40.70])
-  					.scale(60000)
-  					.translate([(width) / 2, (height)/2]);
-    
-    var path = d3.geo.path().projection(projection);
-    svg.append("path")
-        .datum(features)
-        .attr("d", path);
-    
-    //space for tooltip
-    var tooltip = d3.select("body")
-	.append("div")
-    .attr("class","tooltip")
-	.style("position", "absolute")
-	.style("z-index", "10")
-    .style("opacity",0)
-	.style("display", "none");
-    
-    //color the Areas
-    svg.selectAll(".features")
-    .data(topojson.feature(json, json.objects.features).features)
-  .enter().append("path")
-    .attr("class", "NYfeatures")
-    .attr("d", path)
-    .on("mouseover", function(d){
-        tooltip.transition()
-        .duration(200)
-        .style("opacity", .9);
-        //change the details inside the tooltip
-        //if showing general information:
-        if(!DetailedTooltip) {
-            tooltip.html("Borough: "+d.properties.boro_name+"<br/>"+ "Community District: "+ d.id+"<br/>"
-                    +"Population: "+d.properties.population+"<br/>"
-                    +"Life Expectancy: "+d.properties.lifeExpectancy+"<br/>"
-                    +"Income per Capita: "+d.properties.income+"<br/>"
-                    +"Crime: "+d.properties.crimePerK);
-        } else //else show details on topic.
-            tooltip.html("Detailed Info");
-        return tooltip.style("display","inline");
-    })
-    .on("click", function(d){
-        DetailedTooltip=!DetailedTooltip; //toggle.
-        if(!DetailedTooltip) {
-            tooltip.html("Borough: "+d.properties.boro_name+"<br/>"+ "Community District: "+ d.id+"<br/>"
-                    +"Population: "+d.properties.population);
-        } else tooltip.html("Detailed Info");
-    })
-    .on("mousemove", function(d){
-        return tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");
-    })
-    .on("mouseout", function(d){
-        tooltip.transition()
-        .duration(500)
-        .style("opacity", 0);
-       //return tooltip.style("visibility","hidden");
-    });
-});
 //Draw the buttons
-document.write('<button id="Population" class="PopButton" onclick="Population();">Population</button>');
-document.write('<button id="Crime" class="CrimeButton" onclick="Crime();">Crime</button>');
+document.write('<br><button id="Population" class="PopButton" onclick="Population();">Population</button>');
 document.write('<button id="fifeExpectancy" class="LifeButton" onclick="Life();">Life Expectancy</button>');
+document.write('<button id="income" class="IncomeButton" onclick="Income();">Income</button>');
+document.write('<button id="Crime" class="CrimeButton" onclick="Crime();">Crime</button>');
